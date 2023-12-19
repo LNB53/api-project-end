0298fdc759ae81cd276d8dd8c012ba9656ec31cf
Wobr53 <r0898448@student.thomasmore.be>
2023-12-19 16:47:19 +0100

    all games testing completed

--- myproject/test_main.py
+++ myproject/test_main.py
@@ -5,6 +5,7 @@ import requests
 # Test create_player functions - END: POST /players
 def test_create_player():
     headers = {"Content-Type": "application/json"}
+
     payload_1 = {
         "username": "test",
         "email": "t@t.com",
@@ -26,6 +27,7 @@ def test_create_player():
         "country": "BE",
         "password": "abc123!"}
 
+    # Non-existing player
     response_1 = requests.post("http://127.0.0.1:8000/players", json=payload_1, headers=headers)
     assert response_1.status_code == 200
     assert json.loads(response_1.text) == {
@@ -36,11 +38,13 @@ def test_create_player():
         "player_id": 1,
         "progress": []}
 
+    # Already registered username
     response_2 = requests.post("http://127.0.0.1:8000/players", json=payload_2, headers=headers)
     assert response_2.status_code == 400
     response_dict_2 = json.loads(response_2.text)
     assert response_dict_2["detail"] == "Username already registered"
 
+    # Already registered email
     response_3 = requests.post("http://127.0.0.1:8000/players", json=payload_3, headers=headers)
     assert response_3.status_code == 400
     response_dict_3 = json.loads(response_3.text)
@@ -121,6 +125,46 @@ def test_get_player_by_username():
 
 
 # Test create_player functions - END: POST /players
+def test_create_games():
+    headers = {"Content-Type": "application/json"}
+
+    payload_1 = {
+        "title": "game_1",
+        "release_date": "2023-12-19",
+        "genre": "FPS",
+        "developer": "Ubisoft"
+    }
+
+    payload_2 = {
+        "title": "game_1",
+        "release_date": "2022-09-08",
+        "genre": "FPS",
+        "developer": "Ubisoft"
+    }
+
+    # Non-existing game
+    response_1a = requests.post("http://127.0.0.1:8000/games", json=payload_1, headers=headers)
+    assert response_1a.status_code == 200
+    assert json.loads(response_1a.text) == {
+        "title": "game_1",
+        "release_date": "2023-12-19",
+        "genre": "FPS",
+        "developer": "Ubisoft",
+        "game_id": 1,
+        "progress": []
+    }
+
+    # Existing game title with same release date
+    response_1b = requests.post("http://127.0.0.1:8000/games", json=payload_1, headers=headers)
+    assert response_1b.status_code == 400
+    response_dict_1 = json.loads(response_1b.text)
+    assert response_dict_1["detail"] == "Game already registered"
+
+    # Existing game title with different release date
+    response_2 = requests.post("http://127.0.0.1:8000/games", json=payload_2, headers=headers)
+    assert response_2.status_code == 200
+
+
 # Test get_games functions - END: GET /games
 def test_get_games():
     # Unauthenticated
@@ -132,6 +176,23 @@ def test_get_games():
     # Authenticated
     response_2 = requests.get("http://127.0.0.1:8000/games", headers=test_token())
     assert response_2.status_code == 200
+    assert json.loads(response_2.text) == [
+        {
+            "title": "game_1",
+            "release_date": "2023-12-19",
+            "genre": "FPS",
+            "developer": "Ubisoft",
+            "game_id": 1,
+            "progress": []
+        }, {
+            "title": "game_1",
+            "release_date": "2022-09-08",
+            "genre": "FPS",
+            "developer": "Ubisoft",
+            "game_id": 2,
+            "progress": []
+        }
+    ]
 
 
 # Test delete_all function - END: DELETE /reset
